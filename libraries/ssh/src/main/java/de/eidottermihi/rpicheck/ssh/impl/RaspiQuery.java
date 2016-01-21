dc445c2eae0a4280fa9af82ed9ed4f38852e11ce
Michael Prankl <eidottermihi@gmail.com>
2016-01-21 12:57:16 +0100

    Updating copyright headers.

--- libraries/ssh/src/main/java/de/eidottermihi/rpicheck/ssh/impl/RaspiQuery.java
+++ libraries/ssh/src/main/java/de/eidottermihi/rpicheck/ssh/impl/RaspiQuery.java
@@ -1,6 +1,6 @@
 /**
- * Copyright (C) 2015  RasPi Check Contributors
- * 
+ * Copyright (C) 2016  RasPi Check Contributors
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
