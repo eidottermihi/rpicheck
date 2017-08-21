c2fba9d7e6db2a558bef2375fe17fe75e7cdcb44
Michael Prankl <eidottermihi@gmail.com>
2017-08-21 22:06:31 +0200

    Update travis / license / gradle / build tools.

--- app/src/main/java/de/eidottermihi/rpicheck/beans/QueryBean.java
+++ app/src/main/java/de/eidottermihi/rpicheck/beans/QueryBean.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2016  RasPi Check Contributors
+ * Copyright (C) 2017  RasPi Check Contributors
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
