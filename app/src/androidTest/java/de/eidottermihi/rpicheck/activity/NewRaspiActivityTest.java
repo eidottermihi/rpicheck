500d2198908e2dc06f1a2458095e3cdae7ac6c72
Michael Prankl <eidottermihi@gmail.com>
2019-12-21 20:00:51 +0100

    update imports

--- app/src/androidTest/java/de/eidottermihi/rpicheck/activity/NewRaspiActivityTest.java
+++ app/src/androidTest/java/de/eidottermihi/rpicheck/activity/NewRaspiActivityTest.java
@@ -23,9 +23,9 @@
  */
 package de.eidottermihi.rpicheck.activity;
 
+import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.test.suitebuilder.annotation.LargeTest;
 
 import org.junit.Rule;
 import org.junit.Test;
