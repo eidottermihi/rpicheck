95f3510d2fb1ad3d7abcf0fbf11e883aa6a4d233
Michael Prankl <eidottermihi@gmail.com>
2021-05-04 19:52:00 +0200

    Only use getconf if availabe (!212)

--- ssh/src/main/java/de/eidottermihi/rpicheck/ssh/impl/RaspiQuery.java
+++ ssh/src/main/java/de/eidottermihi/rpicheck/ssh/impl/RaspiQuery.java
@@ -76,7 +76,7 @@ public class RaspiQuery implements IQueryService {
     private static final String DF_COMMAND_HEADER_START = "Filesystem";
     private static final String DISTRIBUTION_CMD = "cat /etc/*-release | grep PRETTY_NAME";
     private static final String KERNEL_CMD = "cat /proc/version|cut -f 3 -d \" \"";
-    private static final String ARCHITECTURE_CMD = "(uname -m;echo \" \"; getconf LONG_BIT; echo \"bit\")|tr -d '\\n'";
+    private static final String ARCHITECTURE_CMD = "(uname -m; [ -x \"$(command -v getconf)\" ] && echo \" \" && getconf LONG_BIT && echo \"bit\")|tr -d '\\n'";
     private static final String MODEL_CMD = "cat /sys/firmware/devicetree/base/model";
     private static final String PROCESS_NO_ROOT_CMD = "ps -U root -u root -N";
     private static final String PROCESS_ALL = "ps -A";
